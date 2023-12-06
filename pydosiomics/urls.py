8383e421319ad592b537c9ccdcb82f3b23cc6e8b
siegfriedkollotzek <66626446+siegfriedkollotzek@users.noreply.github.com>
2023-12-06 16:06:42 +0100

    App PyDosiomics

---
+++ pydosiomics/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView
+
+
+urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('', views.index, name='index'),
+    path('index/', views.index, name='index2'),
+
+]
---
+++ pydosiomics/views.py
@@ -0,0 +1,5 @@
+def home(request):
+    return render(request, 'home.html')
+
+def index(request):
+    return render(request, 'index.html')
