0588e87dd082b3af11ad0fe6f9c80e7a28f051ab
Siegfried Kollotzek <66626446+siegfriedkollotzek@users.noreply.github.com>
2025-03-14 09:15:24 +0100

    Update settings.py
    trusted domain dosiomics.com

--- dosiomics/settings.py
+++ dosiomics/settings.py
@@ -142,3 +142,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://dosiomics.com",
+    "https://www.dosiomics.com",  # Include www if needed
+]
