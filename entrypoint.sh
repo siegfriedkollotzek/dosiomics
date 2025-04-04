0d455450303f29f55844e751fee7fae998e52adf
Siegfried Kollotzek <66626446+siegfriedkollotzek@users.noreply.github.com>
2025-04-04 10:11:49 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
-gunicorn dosiomics.wsgi --workers=3 -b 0.0.0.0:80
+gunicorn dosiomics.wsgi --workers=3 --timeout 180 -b 0.0.0.0:80
